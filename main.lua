cd0ec155882908e3d84994f4d6f63c76b96b768a
Gauthier G <gguerisse@icloud.com>
2022-11-08 16:10:36 -0600

    integration macro fastOffset

--- main.lua
+++ main.lua
@@ -1,11 +1,24 @@
 -- FastOffset
 -- By Gauthier GRSS
 
+-- Vous pouvez utiliser fastOffset de 2 manières :
+--      En cliquant simplement sur le plugin dans la pool Plugin, vous pourrez alors modifier l'offset via un pop up
+--      Avec une macro avec un offset pré défini
+
+-- Pour l'utiliser avec une macro il faut une ligne comme celle ci : 'Plugin "fastOffset" "-0.03"'
+-- Cette ligne permet de reculer d'une frame d'offset (une frame à 30 FPS)
+-- Attention la valeur a donner est en seconde !
+
+-- N'oubliez pas les "" pour donner la valeur de l'offset.
+
+-- Afin d'utiliser le plugin, vous devez donner une variable de numéro de track en cours (qui sert de référence au Timecode du track)
+-- Il y a une variable timecodeTrackOffset, cette variable sert si vous avec par exemple le timecode 11 pour le track 1, le 12 pour le track 2... Il faudra alors la mettre a 10
+
 -- Setup :
 local timecodeTrackOffset = 10
 local songNbrVar = 'currenttrackid'
 
-local haveFeedback = true
+local haveFeedback = false
 
 -- Shortcut
 local cmd = gma.cmd
@@ -23,7 +36,7 @@ local function feedback(text)
     if haveFeedback then
         gma.feedback("Plugin fastOffset : " .. text)
     else
-        echo("Plugin fastOffset : " .. text)
+        gma.echo("Plugin fastOffset : " .. text)
     end
 end
 
@@ -66,17 +79,17 @@ local function toAbsoluteOffset(offsetString)
         -- exemple 2:00:00.00
         local patern = "(%d+):(%d+):(%d+).(%d+)"
         local hour, minute, sec, frame = offsetString:match(patern)
-        return (frame * 3.3333 + sec * 100 + minute * 60 * 100 + hour * 60 * 60 * 100) / 100
+        return (frame * 3.333333 + sec * 100 + minute * 60 * 100 + hour * 60 * 60 * 100) / 100
     elseif string.len(offsetString) == 8 or string.len(offsetString) == 7 then
         -- exemple 20:00:00
         local patern = "(%d+):(%d+):(%d+)"
         local minute, sec, frame = offsetString:match(patern)
-        return (frame * 3.3333 + sec * 100 + minute * 60 * 100) / 100
+        return (frame * 3.333333 + sec * 100 + minute * 60 * 100) / 100
     elseif string.len(offsetString) == 5 or string.len(offsetString) == 4 then
         -- exemple 20:00
         local patern = "(%d+):(%d+)"
         local sec, frame = offsetString:match(patern)
-        return (frame * 3.3333 + sec * 100) / 100
+        return (frame * 3.333333 + sec * 100) / 100
     end
 end
 
@@ -93,10 +106,19 @@ local function start(cmdArg)
             feedback('Changement d\'offset annulé !')
         end
     else
+        feedback(cmdArg)
         if string.sub(cmdArg, 1, 1) == '-' then
-            -- Minus
+            local value = string.sub(cmdArg, 2)
+            feedback(toAbsoluteOffset(getOffset(currentTimecode)))
+            feedback(toAbsoluteOffset(getOffset(currentTimecode)) - value)
+            assignOffset(currentTimecode, toAbsoluteOffset(getOffset(currentTimecode)) - value)
+            feedback('Changement d\'offset effectué a ' .. getOffset(currentTimecode))
         else
-            -- Plus
+            local value = cmdArg
+            feedback(toAbsoluteOffset(getOffset(currentTimecode)))
+            feedback(toAbsoluteOffset(getOffset(currentTimecode)) + value)
+            assignOffset(currentTimecode, toAbsoluteOffset(getOffset(currentTimecode)) + value)
+            feedback('Changement d\'offset effectué a ' .. getOffset(currentTimecode))
         end
     end
 end
